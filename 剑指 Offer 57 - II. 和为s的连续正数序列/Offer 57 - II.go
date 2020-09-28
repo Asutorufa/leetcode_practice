06915762228a03aa20d072e34fc4a11cc7270a66
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2020-09-28 18:05:11 +0800

    add Offer 57 - II

---
+++ 剑指 Offer 57 - II. 和为s的连续正数序列/Offer 57 - II.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+/**
+输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+
+示例 1：
+输入：target = 9
+输出：[[2,3,4],[4,5]]
+
+示例 2：
+输入：target = 15
+输出：[[1,2,3,4,5],[4,5,6],[7,8]]
+
+限制：
+    1 <= target <= 10^5
+*/
+
+// 滑动窗口 窗口只想右滑动
+// 大于target就左边界减少 小于target就扩大右边界
+// 从题目中的例子可以知道 左边界最大不会超过target/2 所以左边界到target/2就行了
+func findContinuousSequence(target int) (res [][]int) {
+	i, j := 0, 0
+	sum := 0
+	for i <= target/2 {
+		if sum < target {
+			j++
+			sum += j
+		} else if sum > target {
+			sum -= i
+			i++
+		} else {
+			tmp := []int{}
+			for x := i; x <= j; x++ {
+				tmp = append(tmp, x)
+			}
+			res = append(res, tmp)
+			sum -= i
+			i++
+		}
+	}
+	return
+}
+
+func main() {
+	fmt.Println(findContinuousSequence(9))
+	fmt.Println(findContinuousSequence(15))
+}
