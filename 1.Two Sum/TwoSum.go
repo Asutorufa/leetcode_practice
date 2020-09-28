199e84b730748724c5450bd6f1d80812de46d8b3
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2020-09-28 15:44:55 +0800

    add Offer 57

--- 1.Two Sum/TwoSum.go
+++ 1.Two Sum/TwoSum.go
@@ -19,16 +19,17 @@ var nums = []int{2, 7, 11, 15}
 var target = 9
 
 func twoSum(nums []int, target int) []int {
-    for x := range nums{
-        for y := range nums{
-            if nums[x] == target - nums[y] && x != y{
-				return []int{x,y}
+	for x := range nums {
+		for y := range nums {
+			if nums[x] == target-nums[y] && x != y {
+				return []int{x, y}
 			}
-        }
+		}
 	}
 	return []int{}
 }
 
 func main() {
-	log.Println(twoSum([]int{3,2,4},6))
+	log.Println(twoSum([]int{3, 2, 4}, 6))
+	log.Println(twoSum(nums, target))
 }
